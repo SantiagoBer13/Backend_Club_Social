c4a277506ad4e52160ffc2f8ce3b43f60a999565
Santiago <bernaltinjacasantiago113@gmail.com>
2023-12-12 14:46:28 -0500

    Configurar el Servidor para Permitir CORS

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const app = Express()
 
 app.use(Express.json())
 app.use("/api",newRoutes)
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
 app.use((req,res,next)=>{
     res.status(404).json({
         message: "Endpoint not found"
