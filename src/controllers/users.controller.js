f7da881874564ab527988b816f9cb0519220afc6
Santiago <bernaltinjacasantiago113@gmail.com>
2023-12-26 20:22:02 -0500

    Rutas para crear un usuario y loguerse añadidas

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 import { pool } from "../db.js";
+import bcrypt from 'bcryptjs';
 
 export const getUsers = async (req, res) => {
     try {
@@ -22,6 +23,7 @@ export const getUser = async (req, res) => {
 
 export const createUser = async (req, res) => {
     try {
+        req.body.password = bcrypt.hashSync(req.body.password, 12);
         const {name , surname, username, mail, birthDate, gender, country, city, phone, registerDate, password} = req.body
         const [rows] = await pool.query("INSERT INTO users (name, surname, username,mail,birthDate,gender,country,city,phone,registerDate,password) VALUES (?,?,?,?,?,?,?,?,?,?,?)", 
         [name, surname, username, mail, birthDate, gender, country, city, phone, registerDate, password])
@@ -32,5 +34,30 @@ export const createUser = async (req, res) => {
     } catch (error) {
         return res.status(500).send({ message: "Algo fue mal", messageError:  error});
     }
+}
 
+export const loginUser = async (req, res) => {
+    try {
+        const { mail ,password} = req.body
+        const [rows] = await pool.query("SELECT mail, password FROM users WHERE mail = ?", 
+        [mail])
+        if(rows.length <= 0){
+            return res.status(404).json({message: "Credenciales inválidas"})
+        }
+        const isPasswordValid = bcrypt.compareSync(password, rows[0].password);
+
+        if (!isPasswordValid) {
+            return res.status(404).json({ message: "Credenciales inválidas" });
+        }
+
+        return res.status(200).json({ message: "Inicio de sesión exitoso" });
+        /*
+        return res.status(201).json({
+            id: rows.insertId,
+            name, surname, username, mail, birthDate, gender, country, city, phone, registerDate
+        })
+        */
+    } catch (error) {
+        return res.status(500).send({ message: "Algo fue mal", messageError:  error});
+    }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users.controller.js";
+import { createUser, getUsers, loginUser } from "../controllers/users.controller.js";
 
 const router = Router();
 router.get("/users",getUsers)
 router.get("/users/:id")
 router.post("/user", createUser)
+router.post('/login', loginUser);
 
 export default router;
