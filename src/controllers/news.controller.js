1aba683fb088c7aadd9eb5b22f4619e3a577d98d
Santiago <bernaltinjacasantiago113@gmail.com>
2024-01-24 11:01:25 -0500

    Ruta para obtener eventos por pagina desde el frontend.

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,19 +2,39 @@ import  { pool }  from "../db.js"
 
 export const getNews = async (req, res) => {
     try {
+      //const page = parseInt(req.query.page) || 1;
+      const { page = 1, nameFilter = '' } = req.query;
+      console.log(nameFilter)
+      const startIndex = (page - 1) * 9;
       const [rows] = await pool.query(
-        "SELECT n.id, n.title, n.dateNew, n.description, c.name as category, n.img FROM news n" +
-          " INNER JOIN category c ON c.id = n.idCategory;"
+        `SELECT n.id, n.title, n.dateNew, n.description, c.name AS category, n.img FROM news n 
+          INNER JOIN category c ON c.id = n.idCategory 
+          WHERE n.title LIKE ?
+          ORDER BY n.id
+          LIMIT ?, 9
+        `, [`%${nameFilter}%`, startIndex]
       );
-      return res.json(rows);
+
+      const [numEvents] = await pool.query(
+        `SELECT n.id, n.title, n.dateNew, n.description, c.name AS category, n.img FROM news n 
+          INNER JOIN category c ON c.id = n.idCategory 
+          WHERE n.title LIKE ?
+        `, [`%${nameFilter}%`]
+      );
+
+      const pages = Math.ceil( parseInt(numEvents.length) / 9)
+        console.log(pages)
+      return res.json({"events": rows, "pages": pages})
+      //return res.json(rows);
     } catch (error) {
-      return res.status(500).send({ message: "Error al obtener noticias." });
+      return res.status(500).send({ message: "Error al obtener noticias.", e: error.message});
     }
   };
 
 export const getNew = async (req, res) => {
     try {
         const id = req.params.id
+        
         const [rows] = await pool.query(
             "SELECT n.id, n.title, n.dateNew, n.description, c.name as category,n.img FROM news n"+
         " INNER JOIN category c ON c.id = n.idCategory WHERE n.id = ?",[id])
